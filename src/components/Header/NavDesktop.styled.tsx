94fdb4cf1fdb9fa1f05654efda1515ad19326cb7
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-02-13 17:22:01 -0300

    adiciona dropdown para desktop

--- src/components/Header/NavDesktop.styled.tsx
+++ src/components/Header/NavDesktop.styled.tsx
@@ -2,11 +2,33 @@ import { NavDropdown } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
+type DrowdownProps = {
+  $dropdown: string
+}
+
 export const ImgLogo = styled.img`
   width: 150px;
   height: 50px;
 `
 export const NavItem = styled(Link)`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 1.2rem;
+  color: var(--headerColorText);
+  &:hover {
+    transform: scale(1.05);
+    transition: 0.5s;
+  }
+`
+export const NavitemnNoLinkContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.2rem;
+`
+
+export const NavItemNoLink = styled.div`
   display: flex;
   align-items: center;
   cursor: pointer;
@@ -33,19 +55,8 @@ export const NavDropdownCustom = styled(NavDropdown)`
     background-color: #f2f7ff;
   }
 `
-
-export const NavDropdownItemCustom = styled(NavDropdown.Item)`
-  display: flex;
-  cursor: pointer;
-  text-decoration: none;
-  color: #000;
-  &:hover {
-    font-weight: bold;
-    transition: 0.5s;
-  }
-`
-
 export const Navbar = styled.nav`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -55,5 +66,39 @@ export const Navbar = styled.nav`
 
   p {
     color: var(--headerColorText);
+  }
+`
+// dropdown
+
+export const Dropdown = styled.div<DrowdownProps>`
+  position: absolute;
+  top: 145%; /* Posiciona abaixo do header */
+  width: auto; /* Largura total do elemento pai (NavItemNoLink) */
+  left: 15%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  background-color: var(--headerColorBackground);
+  color: var(--headerColorText);
+  opacity: ${(props) => (props.$dropdown === 'open' ? 1 : 0)};
+  height: ${(props) => (props.$dropdown === 'open' ? 'auto' : 0)};
+  visibility: ${(props) => (props.$dropdown === 'open' ? 'visible' : 'hidden')};
+  gap: 1rem;
+  padding: 1rem;
+
+  transition:
+    opacity 0.3s ease-in-out,
+    height 0.3s ease-in-out,
+    visibility 0.3s ease-in-out;
+`
+
+export const NavItemDropdown = styled(Link)`
+  display: flex;
+  width: auto;
+  cursor: pointer;
+  font-size: 1rem;
+  color: var(--headerColorText);
+  &:hover {
+    border-bottom: none;
   }
 `
--- src/components/Header/NavDesktop.tsx
+++ src/components/Header/NavDesktop.tsx
@@ -1,11 +1,40 @@
-import { Navbar, NavItem } from './NavDesktop.styled'
+import { useState } from 'react'
+import {
+  Dropdown,
+  Navbar,
+  NavItem,
+  NavItemDropdown,
+  NavitemnNoLinkContainer,
+  NavItemNoLink,
+} from './NavDesktop.styled'
+import { MdArrowDropDown, MdArrowDropDownCircle } from 'react-icons/md'
 
 export default function NavDesktop() {
+  const [openDropdown, setOpenDropdown] = useState('closed')
+
+  const toggleDropdown = () => {
+    setOpenDropdown(openDropdown === 'closed' ? 'open' : 'closed')
+  }
   return (
     <>
       <Navbar>
         <NavItem to='/'>Início</NavItem>
-        <NavItem to='/escritorio'>Escritório</NavItem>
+        <NavitemnNoLinkContainer>
+          <NavItemNoLink onClick={toggleDropdown}>Escritório</NavItemNoLink>
+          {openDropdown === 'closed' ? (
+            <MdArrowDropDown />
+          ) : (
+            <MdArrowDropDownCircle />
+          )}
+        </NavitemnNoLinkContainer>
+        <Dropdown $dropdown={openDropdown}>
+          <NavItemDropdown onClick={toggleDropdown} to='/escritorio'>
+            Quem Somos
+          </NavItemDropdown>
+          <NavItemDropdown onClick={toggleDropdown} to='/escritorio'>
+            Áreas de Atuação
+          </NavItemDropdown>
+        </Dropdown>
         <NavItem to='/contatos'>Contatos</NavItem>
         <NavItem to='/servicos'>Plano de Previdência</NavItem>
       </Navbar>
